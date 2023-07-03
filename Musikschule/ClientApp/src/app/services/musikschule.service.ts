bf2a80ce2b1658f31699d780ee3dfaecb1820483
MarkusBach <mb@ehrke-bach.de>
2023-07-03 15:51:53 +0200

    base url

--- Musikschule/ClientApp/src/app/services/musikschule.service.ts
+++ Musikschule/ClientApp/src/app/services/musikschule.service.ts
@@ -2,20 +2,27 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Musikschule } from '../models/musikschule';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MusikschuleService {
 
-  constructor(private readonly _client: HttpClient) { }
+  private _baseUrl = 'http://localhost:5185/api/Musikschule';
+
+  constructor(private readonly _client: HttpClient) { 
+    if(environment.production) {
+      this._baseUrl = '/api/Musikschule';
+    }
+  }
 
   getMusiklschulen(): Observable<Musikschule[]> {
-    return this._client.get<Musikschule[]>('http://localhost:5185/api/Musikschule');
+    return this._client.get<Musikschule[]>(this._baseUrl);
   } 
 
   getMusikschule(id: string): Observable<Musikschule> {
-    return this._client.get<Musikschule>('http://localhost:5185/api/Musikschule/' + id);
+    return this._client.get<Musikschule>(this._baseUrl + '/' + id);
   }
 
   addMusikschule(name: string, adresse: string) {
@@ -23,7 +30,7 @@ export class MusikschuleService {
     item.name = name;
     item.adresse = adresse;
     item.id = "00000000-0000-0000-0000-000000000000";
-    return this._client.post('http://localhost:5185/api/Musikschule', item);
+    return this._client.post(this._baseUrl, item);
   }
 
   editMusikschule(id: string, name: string, adresse: string) {
@@ -31,10 +38,10 @@ export class MusikschuleService {
     item.name = name;
     item.adresse = adresse;
     item.id = id;
-    return this._client.put('http://localhost:5185/api/Musikschule/' + id, item);
+    return this._client.put(this._baseUrl + '/' + id, item);
   }
 
   deleteMusikschule(id: string) {
-    return this._client.delete('http://localhost:5185/api/Musikschule/' + id);
+    return this._client.delete(this._baseUrl + '/' + id);
   }
 }
