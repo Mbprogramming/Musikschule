f1931475c8494f622654159b11d0e0b67c042628
MarkusBach <mb@ehrke-bach.de>
2023-07-04 00:58:54 +0200

    query

---
+++ Musikschule/ClientApp/build-component.js
@@ -0,0 +1,17 @@
+const fs = require('fs-extra');
+const concat = require('concat');
+
+build = async () => {
+    const files = [
+        './dist/query/runtime.js',
+        './dist/query/polyfills.js',
+        './dist/query/main.js'
+    ];
+
+    //   await fs.ensureDir('widget');
+    await concat(files, 'dist/query/query.js');
+    files.forEach(file => {
+        fs.remove(file);
+    });
+}
+build();
